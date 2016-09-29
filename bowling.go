3cccad678728365a122f252c46ac06e9fbbdce01
MedSalahBT <bentaaritmohamedsalah@gmail.com>
2016-09-29 14:33:59 +0200

    tests added

--- bowling.go
+++ bowling.go
@@ -1,5 +1,7 @@
 package bowling
 
+
+
 type Frame struct {
 	firstThrow  int
 	secondThrow int
@@ -7,8 +9,43 @@ type Frame struct {
 
 func GetScore(game []Frame) (int, error) {
 	score := 0
-	for i := 0; i < len(game); i++ {
-		score = score + score[i].firstThrow + score[i].secondThrow
-	}
+
+	if(len(game)!= 10){
+		return 0,nil
+
+	}else{
+		for i := 0; i < len(game); i++ {
+			if(game[i].firstThrow<0 || game[i].secondThrow <0){
+				return 0,nil
+			}else{
+					totalUpplet :=  game[i].firstThrow + game[i].secondThrow
+				if( totalUpplet > 10){
+					return 0,nil
+				}else{
+
+					if(totalUpplet== 10 && i+1 < len(game)){
+
+						if game[i].firstThrow == 10{
+
+							score = score + totalUpplet +game[i+1].firstThrow + game[i+1].secondThrow
+						}else{
+							score = score + totalUpplet +game[i+1].firstThrow 
+						}
+
+						
+						
+					}else{
+						score = score + totalUpplet 
+					}
+
+					
+
+				}
+
+			}
+		
+		}
 	return score, nil
+	}
+	
 }
--- bowling_test.go
+++ bowling_test.go
@@ -26,13 +26,79 @@ func TestNullScore(t *testing.T) {
 }
 
 func TestScore(t *testing.T) {
-	input := []Frame{{7, 5}, {13, -2}}
-	expected := 23
+	input := []Frame{{0,1},{1,2},{0,1},{1,2},{0,1},{1,2},{0,1},{1,2},{0,1},{1,2}}
+	expected := 20
+	
 	if err := scoreChecker(input, expected, nil); err != nil {
 		t.Fatalf("%+v\n", err)
 	}
-	input = []Frame{{0, 2}, {5, 1}}
-	expected = 8
+	
+}
+
+func TestNumberOfIndex(t *testing.T){
+	input := []Frame{{0,1},{1,2},{0,1},{1,2},{0,1},{1,2},{0,1},{1,2},{0,1},{1,2}}
+	expected := 20
+	if err := scoreChecker(input, expected, nil); err != nil {
+		t.Fatalf("%+v\n", err)
+	}
+}
+
+func TestNumberPositif(t *testing.T){
+	input := []Frame{{0,1},{13,2},{0,-1},{13,2},{0,1},{13,2},{0,1},{13,2},{0,1},{13,2}}
+	expected := 0
+	if err := scoreChecker(input, expected, nil); err != nil {
+		t.Fatalf("%+v\n", err)
+	}
+}
+
+func TestSommeTuplet(t *testing.T){
+	input := []Frame{{0,1},{13,2},{0,1},{13,2},{0,1},{13,2},{0,1},{13,2},{0,1},{13,2}}
+	expected := 0
+	if err := scoreChecker(input, expected, nil); err != nil {
+		t.Fatalf("%+v\n", err)
+	}
+}
+
+func TestSpare(t *testing.T) {
+	input := []Frame{{7, 3}, {5, 1}, {3, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}
+	expected := 7+3+5+5+1+3
+
+	if err := scoreChecker(input, expected, nil); err != nil {
+		t.Fatalf("%+v\n", err)
+	}
+}
+
+func TestSpareAtTheEnd(t *testing.T) {
+	input := []Frame{{0, 3}, {5, 1}, {3, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {7,3}}
+	expected := 0+3+5+1+3+7+3
+
+	if err := scoreChecker(input, expected, nil); err != nil {
+		t.Fatalf("%+v\n", err)
+	}
+}
+
+func TestStrike(t *testing.T) {
+	input := []Frame{{10, 0}, {5, 3}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}
+	expected := 10+5+3+5+3
+
+	if err := scoreChecker(input, expected, nil); err != nil {
+		t.Fatalf("%+v\n", err)
+	}
+}
+
+func TestStrikeAtTheEnd(t *testing.T) {
+	input := []Frame{{0, 0}, {6, 1}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {10, 0}}
+	expected := 0+0+6+1+10
+
+	if err := scoreChecker(input, expected, nil); err != nil {
+		t.Fatalf("%+v\n", err)
+	}
+}
+
+func TestDoubleStrike(t *testing.T) {
+	input := []Frame{{10, 0}, {10, 0}, {5, 1}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}
+	expected := 10+10+10+5+1+5+1
+
 	if err := scoreChecker(input, expected, nil); err != nil {
 		t.Fatalf("%+v\n", err)
 	}
